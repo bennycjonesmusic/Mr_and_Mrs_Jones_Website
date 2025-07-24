f12d4dc175aba572d4ddc37a2859c3f32633cd55
Benjamin Connor Jones <bennycjonesmusic@gmail.com>
2025-07-24 22:55:18 +0100

    starting on videos page

---
+++ src/app/Videos/page.tsx
@@ -0,0 +1,39 @@
+import { Navigation } from "../components/Navigation";
+
+
+export default function Videos() {
+
+    return (
+        <>
+            <Navigation />
+            <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1 className="text-4xl font-bold mb-8">Videos</h1>
+                <p className="text-lg mb-4">Check out our latest performances and music videos below!</p>
+                {/* Placeholder for video content */}
+                <div className="flex flex-wrap justify-center gap-8">
+                    {/* Add video components here */}
+                </div>
+            </main>
+        </>
+
+
+
+
+
+
+
+
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+}
